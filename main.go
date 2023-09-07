ef0fefc5094e44217455924e73186ad877a3f164
Zihan <u3231401@uni.canberra.edu.au>
2023-09-08 00:20:37 +1000

    working sku search && database showin

--- main.go
+++ main.go
@@ -1,210 +1,179 @@
 package main
 
 import (
-	"errors"
-	"log"
+	// "errors"
+	// "log"
+	// "fmt"
+	// "database/sql"
+	"fmt"
+	"html/template"
 	"net/http"
 	"plantyplantman/go-htmx-server/database"
 	"strconv"
+
+	// "plantyplantman/go-htmx-server/database"
+	// "strconv"
 	"time"
 
-	g "github.com/maragudk/gomponents"
-	hx "github.com/maragudk/gomponents-htmx"
-	hxhttp "github.com/maragudk/gomponents-htmx/http"
-	c "github.com/maragudk/gomponents/components"
-	. "github.com/maragudk/gomponents/html"
-	ghttp "github.com/maragudk/gomponents/http"
+	// "html/template"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main() {
-	if err := start(); err != nil {
-		log.Fatalln("Error:", err)
-	}
+type ProductTableHeader struct {
+	Header string
 }
 
-func start() error {
-	now := time.Now()
-	mux := http.NewServeMux()
+type ProductTable struct {
+	ProductTableHeaders []ProductTableHeader
+	Products            []database.Product
+}
+
+var PRODUCT_TABLE_HEADERS = []ProductTableHeader{
+				{"Sku"},
+				{"Product Name"},
+				{"Price"},
+				{"Promo Price"},
+				{"Soh"},
+			}
+
+
+func main() {
 	db, err := database.Connect()
 	if err != nil {
-		log.Panicln("Connection to database failed")
+		fmt.Printf("%v\n\n", err)
 	}
-	log.Println("Connection to database succeeded")
+	r := chi.NewRouter()
 
-	mux.HandleFunc("/", ghttp.Adapt(func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
-		if r.Method == http.MethodPost && hxhttp.IsBoosted(r.Header) {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-			products, err := database.GetAllProducts(db, 1, 20)
-			if err != nil {
-				products = []database.Product{}
-			}
+	r.Use(middleware.Timeout(60 * time.Second))
 
-			return ProductTable(products), nil
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		tpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			fmt.Println("err")
 		}
-		return root(now), nil
-	}))
 
-
-	mux.HandleFunc("/products/search", ghttp.Adapt(func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
-		if r.Method == http.MethodPost && hxhttp.IsBoosted(r.Header) {
-			err := r.ParseForm()
-
-			if err != nil {
-				log.Panicln("PARSEFORM FAILED IN POST /products")
-				return nil, err
-			}
-
-			input := r.Form["search-input"][0]
-			hxhttp.SetPushURL(w.Header(), "/products/search?="+input)
-
-			products, err := database.Search(db, "Product", "prodName", input)
-
-			if err != nil {
-				log.Println(err)
-				return nil, err
-			}
-
-			return ProductTable(products), nil
+		products, err := database.GetAllProducts(db, 55, 80)
+		if err != nil {
+			executeErrorTemplate(w, TemplateErrorMessage{Message: "database.GetAllProducts err"}, "database.GetAllProducts err")
 		}
-			return nil, nil
-	}))
 
+		data := ProductTable{
+			ProductTableHeaders: PRODUCT_TABLE_HEADERS,
+			Products: products,
+		}
+		err = tpl.ExecuteTemplate(w, "index.html", data)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			executeErrorTemplate(w, TemplateErrorMessage{fmt.Sprintf("Function: tpl.ExecuteTemplate(index.html)<br>Error: %v", err)},
+				"executeProductTable")
+		}
+	})
 
-	mux.HandleFunc("/products/sku", ghttp.Adapt(func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
-		if r.Method == http.MethodPost && hxhttp.IsBoosted(r.Header) {
-			err := r.ParseForm()
+	r.Get("/products", func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		err = r.ParseForm()
+		if err != nil {
+			executeErrorTemplate(w, TemplateErrorMessage{fmt.Sprintf("Function: r.ParseForm()<br>Error: %v", err)},"parseForm")
+		}
+
+		if sku := r.Form["SkuField"]; len(sku) > 0 && sku[0] != "" {
+			intSku, err := strconv.ParseInt(sku[0], 10, 64)
 			if err != nil {
-				log.Panicln("PARSEFORM FAILED IN POST /products")
+				executeErrorTemplate(w, TemplateErrorMessage{fmt.Sprintf("Function: strconv.ParseUint(sku, 10, 64)<br>Error: %v", err)},"parseForm")
 			}
-
-			sku := r.Form["sku-input"][0]
-			hxhttp.SetPushURL(w.Header(), "/products/sku?="+sku)
-
-			intSku, err := strconv.ParseInt(sku, 10, 64)
-			if err != nil {
-				intSku = 0
-			}
-
-			if intSku == 0 {
-				data, err := database.GetAllProducts(db, 1, 20)
-				if err != nil {
-					return ProductTable([]database.Product{}), nil
-				}
-				return ProductTable(data), nil
-			}
-
 			product, err := database.GetProductFromSku(db, intSku)
 			if err != nil {
-				product = database.Product{}
+				executeErrorTemplate(w, TemplateErrorMessage{fmt.Sprintf("Function: database.GetProductFromSku(db, intSku)<br>Error: %v", err)},"parseForm")
 			}
 
-			retv := []database.Product{product}
-			return ProductTable(retv), nil
-		}
-		return nil, nil
-	}))
+			data := ProductTable{
+				ProductTableHeaders: PRODUCT_TABLE_HEADERS,
+				Products: []database.Product{product},
+			}
 
-	log.Println("Starting on http://localhost:8080")
-	if err := http.ListenAndServe("localhost:8080", mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			err = executeProductTable(w, data)
+			if err != nil {
+				executeErrorTemplate(w, TemplateErrorMessage{fmt.Sprintf("Function: executeProductTable called from /products<br>Error: %v", err)},"parseForm")
+			}
+		}
+		// prodName := r.Form["prodName"]
+	})
+
+
+	http.ListenAndServe(":42069", r)
+}
+
+
+
+func executeProductTable(w http.ResponseWriter, data ProductTable) error {
+	tmplStr := `<table id="ProductTable">
+      <tr id="ProductTableHeaders">
+        {{range .ProductTableHeaders}}
+        <th>{{.Header}}</th>
+        {{end}}
+      </tr>
+
+      <div id="Products">
+        {{range .Products}}
+        <tr id="{{.Sku}}">
+          <td>{{.Sku}}</td>
+          <td>{{.ProdName}}</td>
+          <td>{{.Soh}}</td>
+          <td>{{.Price}}</td>
+          <td>{{.PromoPrice}}</td>
+        </tr>
+        {{end}}
+      </div>
+    </table>`
+
+	tmpl, err := template.New("ProductTable").Parse(tmplStr)
+	if err != nil {
 		return err
 	}
-	return nil
+
+	return tmpl.ExecuteTemplate(w, "ProductTable", data)
 }
 
-const timeFormat = "15:04:05"
-
-func root(now time.Time) g.Node {
-	return c.HTML5(c.HTML5Props{
-		Title: now.Format(timeFormat),
-		Head: []g.Node{
-			Script(Src("https://cdn.tailwindcss.com?plugins=forms,typography")),
-			Script(Src("https://unpkg.com/htmx.org")),
-		},
-		Body: []g.Node{
-			H1(g.Text(`PRODUCTS`)),
-			Div(
-				Class("max-w-7xl mx-auto p-4 prose lg:prose-lg xl:prose-xl"),
-				FormEl(
-					Method("post"),
-					Action("/products/sku"),
-					hx.Boost("true"),
-					hx.Target("#productTable"),
-					hx.Swap("outerHTML"),
-					Label(For("Sku"), g.Text("SKU")),
-					Input(Type("text"), ID("skuInput"), Name("sku-input")),
-					Button(
-						Type("submit"),
-						g.Text(`Get Products`),
-						Class(
-							"rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2",
-						),
-					),
-				),
-			),
-
-			Div(
-				Class("max-w-7xl mx-auto p-4 prose lg:prose-lg xl:prose-xl"),
-				FormEl(
-					Method("post"),
-					Action("/products/search"),
-					hx.Boost("true"),
-					hx.Target("#productTable"),
-					hx.Swap("outerHTML"),
-					Label(For("search"), g.Text("search")),
-					Input(Type("text"), ID("search"), Name("search-input")),
-					Button(
-						Type("submit"),
-						g.Text(`search`),
-						Class(
-							"rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2",
-						),
-					),
-				),
-				ProductTable([]database.Product{}),
-			),
-		},
-	})
+type TemplateErrorMessage struct {
+	Message string
 }
 
-func partial(now time.Time) g.Node {
-	return P(ID("partial"), g.Textf(`Time was last updated at %v.`, now.Format(timeFormat)))
-}
-
-func productItem(product database.Product) g.Node {
-	return Tr(Td(g.Textf("%v", product.Sku)),
-		Td(g.Textf("%v", product.ProdName)),
-		Td(g.Textf("%v", product.Price)),
-		Td(g.Textf("%v", product.PromoPrice)),
-	)
-}
-
-func ProductTableHeaders(isTable bool) g.Node {
-	tableHeadersSlice := []string{
-		"Sku",
-		"Price",
-		"PromoPrice",
-		"Petrie SOH",
-		"Franklin SOH",
-		"Bunda SOH",
-		"Con SOH",
-		"Total SOH",
+func executeErrorTemplate(w http.ResponseWriter, errorMessage TemplateErrorMessage, name string) error {
+	tmplStr := `<div id="ErrorMessage">UHOH<br/>SOMETHING WENT WRONG!<br/>{{.Message}}`
+	tmpl, err := template.New(name).Parse(tmplStr)
+	if err != nil {
+		return err
 	}
-
-	ProductTableHeaders := g.Group(g.Map(tableHeadersSlice, func(s string) g.Node {
-		return Th(g.Text(s))
-	}))
-
-	if isTable {
-		return Table(ProductTableHeaders,ID("productTable"))
-	}
-	return ProductTableHeaders
+	return tmpl.ExecuteTemplate(w, name, errorMessage)
 }
 
-func ProductTable(products []database.Product) g.Node {
+// RESTy routes for "articles" resource
 
-	tableRows := g.Group(g.Map(products, func(product database.Product) g.Node {
-		return productItem(product)
-	}))
-
-	return Table(ID("productTable"), ProductTableHeaders(false), tableRows)
-}
+// r.Route("/articles", func(r chi.Router) {
+//   r.With(paginate).Get("/", listArticles)                           // GET /articles
+//   r.With(paginate).Get("/{month}-{day}-{year}", listArticlesByDate) // GET /articles/01-16-2017
+//
+//   r.Post("/", createArticle)                                        // POST /articles
+//   r.Get("/search", searchArticles)                                  // GET /articles/search
+//
+//   // Regexp url parameters:
+//   r.Get("/{articleSlug:[a-z-]+}", getArticleBySlug)                // GET /articles/home-is-toronto
+//
+//   // Subrouters:
+//   r.Route("/{articleID}", func(r chi.Router) {
+//     r.Use(ArticleCtx)
+//     r.Get("/", getArticle)                                          // GET /articles/123
+//     r.Put("/", updateArticle)                                       // PUT /articles/123
+//     r.Delete("/", deleteArticle)                                    // DELETE /articles/123
+//   })
+// })
+//
+// // Mount the admin sub-router
+// r.Mount("/admin", adminRouter())
